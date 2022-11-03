66aa5fb063903969b8cc9fd9224d5668023f553b
hjkwon <hjkwon0814@naver.com>
2022-11-03 23:17:15 +0900

    create movie controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+
+    @Get()
+    getAll() {
+        return "This will return all movies";
+    }
+
+    @Get("/:id")
+    getOne(@Param('id') movieId : string) {
+        return 'This will return one movies, the id : ${movieId}';
+    }
+}
