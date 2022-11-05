2093686302e24c109dd2e2daec7999d33574195d
hjkwon <hjkwon0814@naver.com>
2022-11-05 23:02:18 +0900

    create movie, remove movie

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -11,5 +11,15 @@ export class MoviesController {
     @Get("/:id")
     getOne(@Param('id') movieId : string) {
         return 'This will return one movies, the id : ${movieId}';
+    }
+
+    @Post()
+    create() {
+        return 'this will create movies';
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') modieId:String) {
+        return "this will delete a movie with the id : ${movieId}";
     }
 }
